2574ae8992815c52c7146cb819a989b007969485
Kiran kumar Gunnammagari <sainathreddy054@gmail.com>
2020-11-17 08:10:02 -0800

    added settimeout

--- src/book-search/BookSearch.test.tsx
+++ src/book-search/BookSearch.test.tsx
@@ -1,59 +1,64 @@
-import React from 'react';
-import { render, fireEvent } from '@testing-library/react';
-import BookSearch from './BookSearch';
+import React from "react";
+import { render, fireEvent } from "@testing-library/react";
+import BookSearch from "./BookSearch";
 
-describe('renders book search', () => {
-    const setup = () => {
-        const utils = render(<BookSearch />)
-        const input = utils.getByPlaceholderText('Search for books to add to your reading list and press Enter') as HTMLInputElement
-        const inputEmpty = utils.getByText('Try searching for a topic, for example') as HTMLInputElement
-        return {
-          input,
-          inputEmpty,
-          ...utils,
-        }
-      }
+describe("renders book search", () => {
+  const setup = () => {
+    const utils = render(<BookSearch />);
+    const input = utils.getByPlaceholderText(
+      "Search for books to add to your reading list and press Enter"
+    ) as HTMLInputElement;
+    const inputEmpty = utils.getByText(
+      "Try searching for a topic, for example"
+    ) as HTMLInputElement;
+    return {
+      input,
+      inputEmpty,
+      ...utils,
+    };
+  };
 
-      test('should allow the letters to be added and deleted in input field', () => {
-        const { input } = setup()
-        fireEvent.change(input, { target: { value: 'java' } })
-        expect(input.value).toBe('java')
-        fireEvent.change(input, { target: { value: '' } })
-        expect(input.value).toBe('')
-      })
+  test("should allow the letters to be added and deleted in input field", () => {
+    const { input } = setup();
+    fireEvent.change(input, { target: { value: "java" } });
+    expect(input.value).toBe("java");
+    fireEvent.change(input, { target: { value: "" } });
+    expect(input.value).toBe("");
+  });
 
-      test('should render text when book is not entered in input field', () => {
-        const { inputEmpty } = setup()
-        expect(inputEmpty).toBeInTheDocument();
-      })
+  test("should render text when book is not entered in input field", () => {
+    const { inputEmpty } = setup();
+    expect(inputEmpty).toBeInTheDocument();
+  });
 
-      test('should check all images rendered when searched by Java', async () => {
-        const { input, ...utils } = setup()
-        fireEvent.change(input, { target: { value: 'java' } })
-        expect(input.value).toBe('java')
-        fireEvent.keyDown(input, { key: 'Enter', code: 'Enter' })
-        await utils.findAllByRole('img')
-        expect(utils.getAllByRole("img").length).toEqual(10)
-      })
+  test("should check all images rendered when searched by Java", async () => {
+    const { input, ...utils } = setup();
+    fireEvent.change(input, { target: { value: "java" } });
+    expect(input.value).toBe("java");
+    fireEvent.keyDown(input, { key: "Enter", code: "Enter" });
+    await utils.findAllByRole("img");
+    expect(utils.getAllByRole("img").length).toEqual(10);
+  });
 
-      test('should render Add this book to wishlist button when searched by java', async () => {
-        const { input, ...utils } = setup()
-        fireEvent.change(input, { target: { value: 'java' } })
-        expect(input.value).toBe('java')
-        fireEvent.keyDown(input, { key: 'Enter', code: 'Enter' })
-        await utils.findAllByRole('button')
-        expect(utils.getAllByRole('button')[1]).toHaveTextContent('Add this book to wishlist')
-      })
+  test("should render Add this book to wishlist button when searched by java", async () => {
+    const { input, ...utils } = setup();
+    fireEvent.change(input, { target: { value: "java" } });
+    expect(input.value).toBe("java");
+    fireEvent.keyDown(input, { key: "Enter", code: "Enter" });
+    await utils.findAllByRole("button");
+    expect(utils.getAllByRole("button")[1]).toHaveTextContent(
+      "Add this book to wishlist"
+    );
+  });
 
-      test('should render My Reading Wishlist heading', async () => {
-        const { input, ...utils } = setup()
-        fireEvent.change(input, { target: { value: 'java' } })
-        expect(input.value).toBe('java')
-        fireEvent.keyDown(input, { key: 'Enter', code: 'Enter' })
-        await utils.findAllByRole('button')
-        fireEvent.click(utils.getAllByRole('button')[1])
-        const linkElement = utils.getByText(/My Reading Wishlist/i);
-        expect(linkElement).toBeInTheDocument();
-      })
-
+  test("should render My Reading Wishlist heading", async () => {
+    const { input, ...utils } = setup();
+    fireEvent.change(input, { target: { value: "java" } });
+    expect(input.value).toBe("java");
+    fireEvent.keyDown(input, { key: "Enter", code: "Enter" });
+    await utils.findAllByRole("button");
+    fireEvent.click(utils.getAllByRole("button")[1]);
+    const linkElement = utils.getByText(/My Reading Wishlist/i);
+    expect(linkElement).toBeInTheDocument();
+  });
 });
--- src/book-search/BookSearch.tsx
+++ src/book-search/BookSearch.tsx
@@ -1,119 +1,125 @@
 import React, { useEffect, useState } from "react";
 import { getBooksByType } from "./book-search.service";
 
-
 const getBookAuthors = (authors: any) => {
-    if (authors && authors.length <= 2) {
-        authors = authors.join(" and ");
-    } else if (authors && authors.length > 2) {
-        let lastAuthor = " and " + authors.slice(-1);
-        authors.pop();
-        authors = authors.join(", ");
-        authors += lastAuthor;
-    }
-    return authors;
-}
+  if (authors && authors.length <= 2) {
+    authors = authors.join(" and ");
+  } else if (authors && authors.length > 2) {
+    let lastAuthor = " and " + authors.slice(-1);
+    authors.pop();
+    authors = authors.join(", ");
+    authors += lastAuthor;
+  }
+  return authors;
+};
 
 const BookSearch = () => {
-    const [bookType, updateBookType] = useState("");
-    const [bookTypeToSearch, updateBookTypeToSearch] = useState("");
-    const [allAvailableBooks, setAllAvailableBooks] = useState({ items: [] });
-    const [wishListBooks, setWishListBooks] = useState<string[]>([])
-    const uniqueWishListBooks = wishListBooks.map((book: any) => book.volumeInfo.title).filter((value, index, self) => self.indexOf(value) === index)
-    async function requestBooks() {
-        if (bookTypeToSearch) {
-            const allBooks = await getBooksByType(bookTypeToSearch);
-            setAllAvailableBooks(allBooks);
-        }
+  const [bookType, updateBookType] = useState("");
+  const [bookTypeToSearch, updateBookTypeToSearch] = useState("");
+  const [allAvailableBooks, setAllAvailableBooks] = useState({ items: [] });
+  const [wishListBooks, setWishListBooks] = useState<string[]>([]);
+  const uniqueWishListBooks = wishListBooks
+    .map((book: any) => book.volumeInfo.title)
+    .filter((value, index, self) => self.indexOf(value) === index);
+  async function requestBooks() {
+    if (bookTypeToSearch) {
+      const allBooks = await getBooksByType(bookTypeToSearch);
+      setAllAvailableBooks(allBooks);
     }
+  }
 
-    useEffect(() => {
-        async function getAllBooks() {
-            await requestBooks();
-        }
-        getAllBooks();
-    }, [bookTypeToSearch]);
-    return (
-            <>
-                <div className="book--container">
-                    <div className="search-params">
-                        <div>
-                            <form
-                                onSubmit={(e) => {
-                                    e.preventDefault();
-                                   updateBookTypeToSearch(bookType)
-                                }}
-                            >
-                                <input
-                                    className="full-width"
-                                    autoFocus
-                                    name="gsearch"
-                                    type="search"
-                                    value={bookTypeToSearch}
-                                    placeholder="Search for books to add to your reading list and press Enter"
-                                    onChange={e => updateBookTypeToSearch(e.target.value)}
-                                />
-                            </form>
-                            {!bookType && (
-                                <div className="empty">
-                                    <p>
-                                        Try searching for a topic, for example
-                                        <a onClick={() => {
-                                                updateBookType("Javascript");
-                                            }}
-                                        >
-                                            {" "}
-                                            "Javascript"
-                                        </a>
-                                    </p>
-                                </div>
-                            )}
-
-                        </div>
-                    </div>
-                </div>
-                <div className="books--wishList">
-           {allAvailableBooks.items && allAvailableBooks.items.length > 0 && <div className="books-list flex-item">
+  useEffect(() => {
+    async function getAllBooks() {
+      await requestBooks();
+    }
+    setTimeout(getAllBooks,500);
+  }, [bookTypeToSearch]);
+  return (
+    <>
+      <div className="book--container">
+        <div className="search-params">
+          <div>
+            <form
+              onSubmit={(e) => {
+                e.preventDefault();
+                updateBookTypeToSearch(bookType);
+              }}
+            >
+              <input
+                className="full-width"
+                autoFocus
+                name="gsearch"
+                type="search"
+                value={bookTypeToSearch}
+                placeholder="Search for books to add to your reading list and press Enter"
+                onChange={(e) => updateBookTypeToSearch(e.target.value)}
+              />
+            </form>
+            {!bookType && (
+              <div className="empty">
+                <p>
+                  Try searching for a topic, for example
+                  <a
+                    onClick={() => {
+                      updateBookType("Javascript");
+                    }}
+                  >
+                    {" "}
+                    "Javascript"
+                  </a>
+                </p>
+              </div>
+            )}
+          </div>
+        </div>
+      </div>
+      <div className="books--wishList">
+        {allAvailableBooks.items && allAvailableBooks.items.length > 0 && (
+          <div className="books-list flex-item">
             {allAvailableBooks.items.map((book: any, index) => {
-                return (
-                    <div key={index}>
-                        <div className="book-details">
-                            <img
-                                alt={`${book.volumeInfo.title} book`}
-                                src={`http://books.google.com/books/content?id=${book.id
-                                    }&printsec=frontcover&img=1&zoom=1&source=gbs_api`}
-                            />
-                            <div>  
-                                <h3>{book.volumeInfo.title}</h3>
-                                <p>Published by: {getBookAuthors(book.volumeInfo.authors)}</p>
-                                <p>published date: {book.volumeInfo.publishedDate}</p>
-                                {/* <p> Description: {book.volumeInfo.description} </p> */}
-                                <button onClick={() => {
-                                    setWishListBooks(wishListBooks => [...wishListBooks, book]);
-                                }}>
-                                    Add this book to wishlist
-                  </button>
-                            </div>
-                        </div>
-                        <hr />
+              return (
+                <div key={index}>
+                  <div className="book-details">
+                    <img
+                      alt={`${book.volumeInfo.title} book`}
+                      src={`http://books.google.com/books/content?id=${book.id}&printsec=frontcover&img=1&zoom=1&source=gbs_api`}
+                    />
+                    <div>
+                      <h3>{book.volumeInfo.title}</h3>
+                      <p>
+                        Published by: {getBookAuthors(book.volumeInfo.authors)}
+                      </p>
+                      <p>published date: {book.volumeInfo.publishedDate}</p>
+                      {/* <p> Description: {book.volumeInfo.description} </p> */}
+                      <button
+                        onClick={() => {
+                          setWishListBooks((wishListBooks) => [
+                            ...wishListBooks,
+                            book,
+                          ]);
+                        }}
+                      >
+                        Add this book to wishlist
+                      </button>
                     </div>
-                );
-            })}
-            </div> }
-            {allAvailableBooks.items && allAvailableBooks.items.length > 0  &&
-                 <div className="books-list flex-item">
-                    <h2> My Reading Wishlist ({uniqueWishListBooks.length}) </h2>
-                    {       
-                        uniqueWishListBooks.map((e: any, key) => {
-                                return <p key={key}> {e} </p>
-                        })
-                    }
+                  </div>
+                  <hr />
                 </div>
-            }
-               </div>
-                
-            </>
-    );
+              );
+            })}
+          </div>
+        )}
+        {allAvailableBooks.items && allAvailableBooks.items.length > 0 && (
+          <div className="books-list flex-item">
+            <h2> My Reading Wishlist ({uniqueWishListBooks.length}) </h2>
+            {uniqueWishListBooks.map((e: any, key) => {
+              return <p key={key}> {e} </p>;
+            })}
+          </div>
+        )}
+      </div>
+    </>
+  );
 };
 
 export default BookSearch;
